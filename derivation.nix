4ae7cf2e70f883d88c5f1167f8301d6a9a7105b3
Jason Felice <jason.m.felice@gmail.com>
2020-10-29 14:51:27 -0400

    Update description

--- derivation.nix
+++ derivation.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
   };
 
   meta = with stdenv.lib; {
-    description = "TODO: fill me in";
+    description = "JIRA syntax files for Kakoune";
     homepage = "https://github.com/eraserhd/kak-jira";
     license = licenses.publicDomain;
     platforms = platforms.all;
